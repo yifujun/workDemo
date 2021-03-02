f54ccbc0428016d3e8ec5b76d10ae7b6c4ea1cbc
yifujun <2577231604@qq.com>
2021-03-02 15:27:44 +0800

    使用Stream流细节修改

--- src/main/java/com/example/Demo1Application.java
+++ src/main/java/com/example/Demo1Application.java
@@ -1,6 +1,5 @@
 package com.example;
 import com.example.thread.SendMailTaskThread;
-import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
